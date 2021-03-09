229ed6480b70475de64ebad275b45235060e5fa4
Gleb Stashinsky <tragy@list.ru>
2021-03-09 23:47:37 +0300

    feat: solve brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let bracket;
+  for(let i=0; i<str.length; i++) {
+    bracket = str.charAt(i);
+    if ( bracket.isSameOpenClose(bracketsConfig) ) {
+      (stack[stack.length-1] === bracket) ? stack.pop() : stack.push(bracket);
+    } else if ( bracket.isOpenBracket(bracketsConfig) ) {
+      stack.push(bracket);
+    } else {
+      if (stack.pop() !== bracket.getOpenBracket(bracketsConfig)) return false;
+    }
+  }
+  return !stack.length;
+}
+
+String.prototype.isOpenBracket = function(bracketsConfig) {
+  for (let i=0; i<bracketsConfig.length; i++) {
+    if (bracketsConfig[i][0] === this.valueOf()) return true;
+  } 
+  return false;
+}
+String.prototype.isSameOpenClose = function(bracketsConfig) {
+  for (let i=0; i<bracketsConfig.length; i++) {
+    if (bracketsConfig[i][0] === this.valueOf() && bracketsConfig[i][0] === bracketsConfig[i][1]) return true;
+  } 
+  return false;
+}
+String.prototype.getOpenBracket = function(bracketsConfig) {
+  for (let i=0; i<bracketsConfig.length; i++) {
+    if (bracketsConfig[i][1] === this.valueOf()) return bracketsConfig[i][0];
+  } 
 }
